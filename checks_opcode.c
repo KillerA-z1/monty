bc4315fd01ddb9ae34c7b0d48cbc56955918f8bd
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-06-23 03:05:24 +0300

    checks opcode and returns the correct function

---
+++ checks_opcode.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * get_op_func -  checks opcode and returns the correct function
+ * @str: the opcode
+ * Return: returns a functions, or NULL on failure
+ */
+instruct_func get_op_func(char *str)
+{
+	int i;
+
+	instruction_t instruct[] = {
+		{NULL, NULL},
+	};
+
+	i = 0;
+	while (instruct[i].f != NULL && strcmp(instruct[i].opcode, str) != 0)
+	{
+		i++;
+	}
+
+	return (instruct[i].f);
+}
