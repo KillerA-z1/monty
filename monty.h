06cdf30de25586710c6a71d1ab9a715a61941889
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-06-23 03:06:37 +0300

    hot fix

--- monty.h
+++ monty.h
@@ -40,6 +40,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+instruct_func get_op_func(char *str);
 void read_file(char *filename, stack_t **stack);
 int _isalpha(int c);
 int isnumber(char *str);
