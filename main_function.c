1624f34a98dba1761dff19d9be9f9a1cf31bf626
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-06-23 03:25:03 +0300

    hot fix 00

--- main_function.c
+++ main_function.c
@@ -18,3 +18,45 @@ void pint_func(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", runner->n);
 }
+
+/**
+ * _pall - print all function
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void pall_func(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *runner;
+
+	runner = *stack;
+	while (runner != NULL)
+	{
+		printf("%d\n", runner->n);
+		runner = runner->next;
+	}
+}
+/**
+ * push_func - to push int to a stack
+ * @stack: the linked lists for monty stack
+ * @line_number: number of line opcode occurs
+ */
+
+void push_func(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *top;
+	(void)line_number;
+
+	top = malloc(sizeof(stack_t));
+	if (top == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	top->n = var_global.push_arg;
+	top->next = *stack;
+	top->prev = NULL;
+	if (*stack != NULL)
+		(*stack)->prev = top;
+	*stack = top;
+}
